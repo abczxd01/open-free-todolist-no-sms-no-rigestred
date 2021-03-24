17511937ecab90e9545d382af7a518d49446ad8d
Alexander <jinmaster707@gmail.com>
2021-03-24 23:14:15 +0600

    Added basic logic task List

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 "use strict"
-console.log('hi')
-import "./scss/style.scss"
+import "./sass/style.scss"
+let inputCreateTask = document.querySelector('.input__creatTask')
+
+let tasks = []
+
+class Task {
+    constructor(title, text, date) {
+        // this.id = 0
+        this.title = title
+        this.text = text
+        this.date = date
+    }
+}
+
+function createTask(objTask) {
+    tasks.push(objTask)
+    let taskHTML = document.createElement('div')
+    taskHTML.classList.add('task')
+    taskHTML.innerHTML = objTask.text
+    let checkBox = document.createElement('input')
+    checkBox.type = "checkbox"
+    taskHTML.append(checkBox)
+    document.body.append(taskHTML)
+}
+
+
+inputCreateTask.addEventListener('keyup', (event) => {
+    if (event.keyCode === 13) {
+        createTask(new Task('', inputCreateTask.value, '22.02.2001'))
+    }
+})
+
+console.log(tasks)
