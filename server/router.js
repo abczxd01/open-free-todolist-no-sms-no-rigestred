90fad29ced89a7ef058be37f107f55c190815c00
Alexander <jinmaster707@gmail.com>
2021-03-28 22:15:55 +0600

    The basic structure of the server is done

---
+++ server/router.js
@@ -0,0 +1,12 @@
+import Router from "express";
+import TaskController from "./TaskController.js"
+
+const router = new Router()
+
+router.post('/tasks',TaskController.create)
+router.get('/tasks', TaskController.getAll)
+router.get('/tasks/:id',TaskController.getOne)
+router.put('/tasks',TaskController.update)
+router.delete('/tasks/:id',TaskController.delete)
+
+export default router
