8c71a298ada9c2e7a65543510f7f10d3a0817c3c
Alexander <jinmaster707@gmail.com>
2021-04-22 13:35:53 +0600

    Test version of search header

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,15 +1,35 @@
 /* eslint-disable no-restricted-syntax */
-const headerSearchInput = document.querySelector('.header__search-input');
-const headerSearch = document.querySelector('.header__search');
+let headerSearchInput = null;
 const headerWrapper = document.querySelector('.header__wrapper');
+const headerButtons = document.querySelectorAll('.header__button');
+
+function headerSearch(event) {
+  if (event.code.toLowerCase() === 'enter') {
+    headerButtons.forEach((element) => {
+      if (!element.matches('.header__search')) element.classList.toggle('header__hide');
+    });
+    headerSearchInput.remove();
+    headerSearchInput.removeEventListener('keyup', headerSearch);
+  }
+}
 
 function searchGeneration() {
   const input = document.createElement('input');
   input.setAttribute('type', 'text');
   input.classList.add('header__search-input');
-  input.style.width = `${headerWrapper.offsetWidth}px`;
-  input.style.height = `${headerWrapper.offsetHeight}px`; 
-  headerWrapper.replaceWith(input);
+  input.style.width = `${headerWrapper.offsetWidth - 66}px`;
+  headerButtons.forEach((element) => {
+    if (!element.matches('.header__search')) element.classList.toggle('header__hide');
+  });
+  headerSearchInput = document.querySelector('.header__search-input');
+  if (headerSearchInput === null) {
+    headerWrapper.prepend(input);
+    headerSearchInput = document.querySelector('.header__search-input');
+    headerSearchInput.addEventListener('keyup', headerSearch);
+  } else {
+    headerSearchInput.remove();
+    headerSearchInput.removeEventListener('keyup', headerSearch);
+  }
 }
 
-headerSearch.addEventListener('click', searchGeneration);
+document.querySelector('.header__search').addEventListener('click', searchGeneration);
